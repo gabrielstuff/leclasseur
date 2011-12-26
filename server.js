f0392afc462e5517373e05f1e2cdb76748bc9b4c
Gabriel <gabriel@soixantecircuits.fr>
2011-12-26 14:59:27 +0000

    adding CSS bootstrap and first template with jade

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  });
 });
 
 var port = process.env.PORT || 3000;
+app.set('view engine', 'jade');
+app.use(express.staticProvider(__dirname + '/public'));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
